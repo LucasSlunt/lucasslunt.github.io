ee737e02139da68de4023ee429594eb60b1101d5
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-04-20 23:13:44 -0700

    Script to customize header text

---
+++ virtualworldscript.js
@@ -0,0 +1,5 @@
+function main() {
+    document.getElementById('project-date').innerHTML = 'April 2025';
+    document.getElementById('project-title').innerHTML = 'Virtual World';
+    document.getElementById('project-description').innerHTML = 'A vast open world full of nature and surprises. Made with Unity, Gaia, and Blender';
+}
