6203ff41b028e1986005b2bf82872355dad1da32
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-04-21 15:06:20 -0700

    more pictures

--- virtualworldscript.js
+++ virtualworldscript.js
@@ -1,5 +1,13 @@
-function main() {
+function projectTitle() {
     document.getElementById('project-date').innerHTML = 'April 2025';
     document.getElementById('project-title').innerHTML = 'Virtual World';
     document.getElementById('project-description').innerHTML = 'A vast open world full of nature and surprises. Made with Unity, Gaia, and Blender';
+}
+
+var leftImageIndex = 0;
+var imageDirectory = "./virtual_world_media/"
+var images = "[Berries.png"]
+
+function nextImages(){
+
 }
