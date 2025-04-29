4770beb298b24b09c749f4ddf63794e530de2398
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-04-29 15:24:15 -0700

    Next button works

--- applesscript.js
+++ applesscript.js
@@ -1,8 +1,11 @@
-var AppleCounter = 0;
+var AppleCounter;
 var imageDirectory = "./ApplePictures/";
 var appleImages = ["AuroraGoldenGala","EgremontRusset","Kanzi","Kissabel","LucyGlo","Orin","ScarlettParadise"];
 var fileFormat = ".png";
-var appleImage;
+var mainImage;
+var htmlElementsArray = [];
+var numOfApples = appleImages.length;
+
 
 function setupPage(){
     initializeImages();
@@ -10,22 +13,37 @@ function setupPage(){
 }
 
 function initializeImages(){
-    appleImage = document.getElementById('apple-picture');
+    
+    htmlElementsArray[0] = document.getElementById('apple1'); 
+    htmlElementsArray[1] = document.getElementById('apple2');
+    htmlElementsArray[2] = document.getElementById('apple3');
 
-    nextApple();
+    mainImage = document.getElementById('apple-picture');
+
+    htmlElementsArray[3] = document.getElementById('apple4');
+    htmlElementsArray[4] = document.getElementById('apple5');
+    htmlElementsArray[5] = document.getElementById('apple6');
     
 }
 
 function nextApple(){
-    if (AppleCounter < appleImages.length-1) AppleCounter++;
+    if (AppleCounter < numOfApples-1) AppleCounter++;
     else                                   AppleCounter = 0;
-    appleImage.src = imageDirectory+appleImages[AppleCounter]+fileFormat;
+    mainImage.src = imageDirectory+appleImages[AppleCounter]+fileFormat;
+
+    htmlElementsArray[0].src = imageDirectory+appleImages[AppleCounter-3]+fileFormat;
+    htmlElementsArray[1].src = imageDirectory+appleImages[AppleCounter-2]+fileFormat;
+    htmlElementsArray[2].src = imageDirectory+appleImages[AppleCounter-1]+fileFormat;
+
+    htmlElementsArray[3].src = imageDirectory+appleImages[AppleCounter+1]+fileFormat;
+    htmlElementsArray[4].src = imageDirectory+appleImages[AppleCounter+2]+fileFormat;
+    htmlElementsArray[5].src = imageDirectory+appleImages[AppleCounter+3]+fileFormat;
 }
 
 function prevApple(){
     if (AppleCounter > 0) AppleCounter--;
-    else                  AppleCounter = appleImages.length-1;
-    appleImage.src = imageDirectory+appleImages[AppleCounter]+fileFormat;
+    else                  AppleCounter = numOfApples-1;
+    mainImage.src = imageDirectory+appleImages[AppleCounter]+fileFormat;
 }
 
 function setupButtons(){
