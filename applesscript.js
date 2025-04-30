89e22681e5cbc1d4db3ef67e44531ec24360112c
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-04-29 17:10:17 -0700

    apple name is displayed

--- applesscript.js
+++ applesscript.js
@@ -1,8 +1,10 @@
 var AppleCounter;
 var imageDirectory = "./ApplePictures/";
 var appleImages = ["AuroraGoldenGala","EgremontRusset","Kanzi","Kissabel","LucyGlo","Orin","ScarlettParadise"];
+var appleNames = ["Aurora Golden Gala","Egremont Russet","Kanzi","Kissabel","Lucy Glo","Orin","Scarlett Paradise"];
 var fileFormat = ".png";
 var mainImage;
+var mainAppleTitle;
 var htmlElementsArray = [];
 var numOfApples = appleImages.length;
 
@@ -23,19 +25,27 @@ function initializeImages(){
     htmlElementsArray[3] = document.getElementById('apple4');
     htmlElementsArray[4] = document.getElementById('apple5');
     htmlElementsArray[5] = document.getElementById('apple6');
+
+    mainAppleTitle = document.getElementById('main-apple-title');
     
+    nextApple();
 }
 
 function nextApple(){
     if (AppleCounter < numOfApples-1) AppleCounter++;
     else                                   AppleCounter = 0;
     displayApplePictures();
+    changeAppleDescription();
 }
 
 function prevApple(){
     if (AppleCounter > 0) AppleCounter--;
     else                  AppleCounter = numOfApples-1;
     displayApplePictures();
+}
+
+function changeAppleDescription(){
+    mainAppleTitle.innerHTML = appleNames[AppleCounter];
 }
 
 function displayApplePictures(){
