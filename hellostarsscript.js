c59d7d5e473613bcc0c4afc81618df278e5433be
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-05-10 11:05:42 -0700

    setup for animated background

--- hellostarsscript.js
+++ hellostarsscript.js
@@ -1,4 +1,4 @@
 function setupPage(){
     colourNavbar("rgb(196, 28, 140)","rgb(223, 62, 132)");
-    loadProjectHeader('March 2025','Hello, Stars!','First-place winner of UBCOs GIT Hackathon, densely packed with engaging educational features',"rgb(223, 62, 132)");
+    loadProjectHeader('March 2025','Hello, Stars!','First-place winner of UBCOs GIT Hackathon. Made with Java, MySQL, Springboot, Python, JavaScript, HTML, and CSS',"rgb(223, 62, 132)");
 }
