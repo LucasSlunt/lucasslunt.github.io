42fc27fa389aca8a635e663539ec1e0eebbd8f2f
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-05-10 10:06:11 -0700

    added header and navbar code

---
+++ hellostarsscript.js
@@ -0,0 +1,4 @@
+function setupPage(){
+    colourNavbar("rgb(124, 161, 173)","lightblue");
+    loadProjectHeader('April 2025','Virtual World','A vast open world full of nature and surprises. Made with Unity, Gaia, and Blender',"lightblue");
+}
