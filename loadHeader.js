c029fa49f526c8a59ce9da858a9b3859a65d7287
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-05-09 17:43:00 -0700

    refactored header on virtual world

---
+++ loadHeader.js
@@ -0,0 +1,5 @@
+function loadProjectHeader(date,title,description) {
+    document.getElementById('project-date').innerHTML = date;
+    document.getElementById('project-title').innerHTML = title;
+    document.getElementById('project-description').innerHTML = description;
+}
--- virtualworldscript.js
+++ virtualworldscript.js
@@ -1,13 +1,7 @@
 function setupPage(){
-    projectTitle();
+    loadProjectHeader('April 2025','Virtual World','A vast open world full of nature and surprises. Made with Unity, Gaia, and Blender');
     initializeImages();
     setupButtons();
-}
-
-function projectTitle() {
-    document.getElementById('project-date').innerHTML = 'April 2025';
-    document.getElementById('project-title').innerHTML = 'Virtual World';
-    document.getElementById('project-description').innerHTML = 'A vast open world full of nature and surprises. Made with Unity, Gaia, and Blender';
 }
 
 var ImageSetCounter = 3;
