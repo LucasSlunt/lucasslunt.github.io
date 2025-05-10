92160024d3e5ceb9f8f82cd2fff75a424032e524
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-05-09 18:55:29 -0700

    added navbar to VW page

--- virtualworldscript.js
+++ virtualworldscript.js
@@ -1,4 +1,5 @@
 function setupPage(){
+    colourNavbar("rgb(124, 161, 173)","lightblue");
     loadProjectHeader('April 2025','Virtual World','A vast open world full of nature and surprises. Made with Unity, Gaia, and Blender',"lightblue");
     initializeImages();
     setupButtons();
