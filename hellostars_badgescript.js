f00f9f94a2ad77964c98f8c0c8b31d10fb298adc
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-05-11 10:06:13 -0700

    badge changes on timer

--- hellostars_badgescript.js
+++ hellostars_badgescript.js
@@ -1,13 +1,17 @@
 function setBadgeLevel(level){
-    if (level == "silver"){
+    var col1 = "#000000";
+    var col2 = "#000000";
+    var col3 = "#000000";
+    
+    if (level == 0){//bronze
         col1 = "#3f3a39";
         col2 = "#dddce3";
         col3 = "#9194ac";
-    }else if (level == "bronze"){
+    }else if (level == 1){//silver
         col1 = "#501c07";
         col2 = "#cc852d";
         col3 = "#774a20";
-    }else if (level == "gold"){
+    }else if (level == 2){//gold
         col1 = "#b5640b";
         col3 = "#f7bd02";
         col2 = "#fff038";
--- hellostarsscript.js
+++ hellostarsscript.js
@@ -1,5 +1,32 @@
+var isBadgeAnimated = true;
+
 function setupPage(){
     colourNavbar("rgb(196, 28, 140)","rgb(223, 62, 132)");
     loadProjectHeader('March 2025','Hello, Stars!','First-place winner of UBCOs GIT Hackathon. Made with Java, MySQL, Springboot, Python, JavaScript, HTML, and CSS',"rgb(223, 62, 132)");
     setBadgeLevel("silver");
+    animateBadge();
+}
+
+function animateBadge(){
+    var randomNum = Math.floor(Math.random()*3);
+    setBadgeLevel(randomNum);
+        var intervalId = null;
+        var timer = 0;
+        clearInterval(intervalId);
+
+        intervalId = setInterval(frame, 5);
+        function frame() {
+          if (timer == 400) {
+            timer = 0;
+            clearInterval(intervalId);
+            intervalId = setInterval(frame, 5);
+
+            var randomNum = Math.floor(Math.random()*3);
+            setBadgeLevel(randomNum);
+
+          } else {
+            timer++;
+          }
+        }
+
 }
