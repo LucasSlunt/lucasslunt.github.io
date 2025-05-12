5346b84e9113e4e3dbda9ab88e5f650300769d99
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-05-12 11:47:47 -0700

    added cycling image to badge

--- hellostars_badgescript.js
+++ hellostars_badgescript.js
@@ -1,8 +1,10 @@
+var numOfBadgeImages = 5;
+
 function setBadgeLevel(level){
     var col1 = "#000000";
     var col2 = "#000000";
     var col3 = "#000000";
-    
+
     if (level == 0){//bronze
         col1 = "#3f3a39";
         col2 = "#dddce3";
@@ -17,18 +19,41 @@ function setBadgeLevel(level){
         col2 = "#fff038";
     }
 
-    var badgeItems1 = document.getElementsByClassName("badge-colour-1")
+    var badgeItems1 = document.getElementsByClassName("badge-colour-1");
     for (var i = 0; i < badgeItems1.length; i++) {
         badgeItems1[i].style.fill = col1;
     }
 
-    var badgeItems2 = document.getElementsByClassName("badge-colour-2")
+    var badgeItems2 = document.getElementsByClassName("badge-colour-2");
     for (var i = 0; i < badgeItems2.length; i++) {
         badgeItems2[i].style.fill = col2;
     }
 
-    var badgeItems3 = document.getElementsByClassName("badge-colour-3")
+    var badgeItems3 = document.getElementsByClassName("badge-colour-3");
     for (var i = 0; i < badgeItems3.length; i++) {
         badgeItems3[i].style.fill = col3;
     }
+
+    var innerBadgeImage = document.getElementById("inner-badge-image");
+    var imageTitle = "";
+    var randomImageSelector = Math.floor(Math.random()*numOfBadgeImages);
+    switch (randomImageSelector){
+        case 0:
+            imageTitle = "centaurus";
+            break;
+        case 1:
+            imageTitle = "crux";
+            break;
+        case 2:
+            imageTitle = "leo";
+            break;
+        case 3:
+            imageTitle = "musca";
+            break;
+        case 4:
+        default:
+            imageTitle = "ursa-minor";
+            break;    
+    }
+    innerBadgeImage.src = "./hello_stars_media/"+imageTitle+".png";
 }
