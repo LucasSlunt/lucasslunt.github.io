777954287cb559448be07dcdd633f1df22e2fdeb
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-05-19 10:53:45 -0700

    toggle table button functionality

--- arkusfishingscript.js
+++ arkusfishingscript.js
@@ -1,4 +1,21 @@
 function setupPage(){
     loadProjectHeader('August 2023','Arkus\'s Fishing','An exciting expansion of Minecrafts fishing mechanics. Made with the Minecraft Forge API, Java, and GIMP.','bisque');
     colourNavbar("rgb(245, 215, 135)","bisque");
+    setupButtons();
+}
+
+function setupButtons(){
+
+    document.getElementById("item-table-button").addEventListener('click', function () {
+        toggleTable("item-table");});
+}
+
+function toggleTable(tableID){
+    console.debug("toggle table called, " + document.getElementById(tableID).style.display);
+    if (document.getElementById(tableID).style.display == "none") {
+        document.getElementById(tableID).style.display = "block";
+    }
+    else{
+        document.getElementById(tableID).style.display = "none";
+    }
 }
