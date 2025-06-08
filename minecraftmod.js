b786b891d5e4e0b7a355df4f63271730721963e1
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-06-08 11:29:56 -0700

    changed structure of file folders
    each page gets its own folder, reducing clutter. The next commits will fix all broken links resulting from this

--- minecraftmod.js
+++
@@ -1,4 +0,0 @@
-function setupPage(){
-    loadProjectHeader('August 2023','Arkus\'s Fishing','An exciting expansion of Minecrafts fishing mechanics. Made with the Minecraft Forge API, Java, and GIMP.','bisque');
-    colourNavbar("rgb(245, 215, 135)","bisque");
-}
