466bc38e4f248d5eb5bacc5e360a8567bee2412c
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-06-08 12:04:14 -0700

    removed header scripts
    The project title is the first thing someone will see on a page, so it's important that it doesn't take long to load

--- pages/arkusfishing/arkusfishingscript.js
+++ pages/arkusfishing/arkusfishingscript.js
@@ -1,5 +1,4 @@
 function setupPage(){
-    loadProjectHeader('August 2023','Arkus\'s Fishing','An exciting expansion of Minecrafts fishing mechanics. Made with the Minecraft Forge API, Java, and GIMP.','bisque');
     colourNavbar("rgb(245, 215, 135)","bisque");
     setupButtons();
 }
