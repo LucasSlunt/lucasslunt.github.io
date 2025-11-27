356c3b9fb509cb9c278abd575309657a75a22a6e
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-11-27 14:05:01 -0800

    Added new apple data

--- pages/apples/applesTableScript.js
+++ pages/apples/applesTableScript.js
@@ -101,7 +101,23 @@ function initializeTableVariables(){
     tApples.push(["Img","Bliss","Sweet, slightly tart","Soft",4,"Little information available online.","Unknown","January 2, 2025",81]);
     tApples.push(["Img","Autumn Glory","Sweet, cinnamon, caramel","Very crisp",9,"Excellent for applesauce, or savoury dishes","Golden Delicious x Fuji","March 6, 2025",82]);
     tApples.push(["Img","Mutsu (Crispin)","Very sour","Crisp",3,"Changes colour based on the amount of sunlight it is exposed to","Indo x Golden Delicious","September 13, 2025",83]);
-
+    tApples.push(["Img","Snow Cloud","Tart, Sweet","Hard, crisp",4,"A popular ornamental crabapple, the size of a dime","Strawberry Parfait x Crimson Cloud","November 5, 2025",84]);
+    tApples.push(["Img","Elstar","Sweet, honeyed","Softer",7,"Very popular in Europe","Golden Delicious x Ingrid Marie","November 6 2025",85]);
+    tApples.push(["Img","Winter Banana","Mildly sweet, faint notes of banana and topical fruit","Softer",7,"Smells like a banana when ripe","Unknown","November 7 2025",86]);
+    tApples.push(["Img","Easter Orange","Sweet, intense","Dry, dense, crisp",6,"Very orange skin","Unknown","November 8 2025",87]);
+    tApples.push(["Img","Golden Russet","Subtly sweet, tart, molasses, caramel, spice","Very dense, tough",7,"Very strong flavour, generally used for cider","Unknown","November 9 2025",88]);
+    tApples.push(["Img","Lady (Api)","sweet, bright, citrus, floral","Fairly crisp",7,"A 400 year old variety, historically used in Christmas wreaths and garlands","Unknown","November 10 2025",89]);
+    tApples.push(["Img","Winesap","tangy, sweet, vinous","softer",6,"Often used for cider","Unknown","November 11 2025",90]);
+    tApples.push(["Img","Sekai Ichi (World #1)","Very sweet, light","Very crisp",6,"Sekai Ichi means World's Number One in Japanese","Golden Delicous x Red Delicious","November 12 2025",91]);
+    tApples.push(["Img","Discovery","Mildly sweet","Soft, smooth, almost buttery",7,"Generally used for applesauce, and exceedingly fit to do so","Beauty of Bath x Worcester Pearmain","November 13 2025",92]);
+    tApples.push(["Img","Crawley Beauty","Light, slightly sour","softer",5,"Blindingly white flesh","Unknown","November 14 2025",93]);
+    tApples.push(["Img","Chinook","Refreshingly tart, balanced","Firm, crisp",5,"Created in Summerland B.C. alongside the Silken and Creston varieties","Gala x Splendour","November 15 2025",94]);
+    tApples.push(["Img","Johnathan","Light, slightly sour","Soft",4,"Used to be very popular but has been overtaken by modern varieties","Esopus Spitzenburg x Unknown","November 16 2025",95]);
+    tApples.push(["Img","Court Royal","Sweet, complex","Soft",5,"Not to be confused with Royal Court, a sport of Cortland","Unknown","November 17 2025",96]);
+    tApples.push(["Img","Snow Apple (Famuese)","Barely tart, delicately vinous","Soft",3,"The 100th variety I've tried","Unknown","November 18 2025",97]);
+    tApples.push(["Img","Dazzle","Juicy, sweet","Very crisp, like a watermelon",7,"Very popular in Asia","Scired x Sweetie","November 19 2025",98]);
+    tApples.push(["Img","Gloster 69","Mildy sweet, barely tart","Crisp, a bit mealy",4,"Released in 1969 (hence the name)","Glockenapfel x Richared Delicious","November 20 2025",99]);
+    tApples.push(["Img","Evercrisp","Ridiculously sweet, tart","Very crunchy, like splitting wood",7,"A very overpowering and intense apple","A x B","November 21 2025",100]);
 
     //tApples.push(["Img","name","taste","texture",rating,"note","A x B","date",number]);
     addimagesToArray(tApples);
